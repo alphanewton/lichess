8382ea910b7b86f930705897135b81485e447ec2
newton <newtonnarzary28@gmail.com>
2024-03-02 01:36:13 +0530

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ const Home = () => {
     const fetchTopPlayers = async () => {
       try {
         const response = await axios.get("https://lichess.org/api/player");
-        setTopPlayersData(response.data[selectedCategory].slice(0, 10));
+        setTopPlayersData(response.data[selectedCategory]);
       } catch (error) {
         console.error("Error fetching top players data:", error.message);
       }
@@ -32,7 +32,6 @@ const Home = () => {
         const response = await axios.get(
           "https://lichess.org/api/streamer/live"
         );
-        console.log(response.data);
         setLivestreams(response.data);
       } catch (error) {
         console.error("Error fetching livestream data:", error.message);
@@ -49,150 +48,258 @@ const Home = () => {
     setSearchResults(filteredPlayers);
   };
 
+  async function getLinks(userid: String) {
+    try {
+      const response = await axios.get(
+        `https://lichess.org/api/user/${userid}`
+      );
+
+      const temp = {};
+      temp["url"] = response.data.url;
+
+      if (response.data.playing) {
+        temp["playing"] = response.data.playing;
+      }
+
+      if (response.data.streamer) {
+        if (response.data.streamer.twitch) {
+          temp["twitch"] = response.data.streamer.twitch;
+        }
+        if (response.data.streamer.youtube) {
+          temp["youtube"] = response.data.streamer.youtube;
+        }
+      }
+
+      return temp;
+    } catch (error) {
+      console.error("Error fetching livestream data:", error.message);
+    }
+  }
+
   return (
     <div>
       <Head>
-        <title>Lichess Player Tracker</title>
+        <title>Lichess Player Tracker - Built by Newton</title>
         <meta name="description" content="Track top players on Lichess" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-3xl mb-4">Lichess Player Tracker</h1>
-
-        {/* Search Players */}
-        <section>
-          <h2 className="text-xl mb-2">Search Players</h2>
-          <div className="mb-4">
-            <input
-              type="text"
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-3xl mb-4">
+          Lichess Player Tracker - Built by Newton
+        </h1>
+        <div className="flex space-x-11">
+          <section>
+            <select
               className="border p-2 text-black"
-              placeholder="Search for a player..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
-            <button
-              className="bg-blue-500 text-white p-2 ml-2"
-              onClick={handleSearch}
+              value={selectedCategory}
+              onChange={(e) => {
+                setSelectedCategory(e.target.value);
+              }}
             >
-              Search
-            </button>
-          </div>
-          <ul>
-            {searchResults.map((player, index) => (
-              <li key={index} className="mb-2">
-                <div>Name: {player.id}</div>
-                <div>Name: {player.username}</div>
-                <div>
-                  Progress:{" "}
-                  {player.perfs[selectedCategory]
-                    ? player.perfs[selectedCategory].progress
-                    : ""}
-                </div>
-                <div>
-                  Rating:{" "}
-                  {player.perfs[selectedCategory]
-                    ? player.perfs[selectedCategory].rating
-                    : ""}
-                </div>
-              </li>
-            ))}
-          </ul>
-        </section>
+              <option value="bullet">Bullet</option>
+              <option value="blitz">Blitz</option>
+              <option value="rapid">Rapid</option>
+              <option value="classical">Classical</option>
+              <option value="ultraBullet">Ultra Bullet</option>
+              <option value="chess960">Chess960</option>
+              <option value="crazyhouse">Crazy House</option>
+              <option value="antichess">Antichess</option>
+              <option value="atomic">Atomic</option>
+              <option value="horde">Horde</option>
+              <option value="kingOfTheHill">King Of The Hill</option>
+              <option value="racingKings">Racing Kings</option>
+              <option value="threeCheck">Three Check</option>
+            </select>
+          </section>
 
-        {/* Option Bar to Choose Category */}
+          <section>
+            <div className="mb-4">
+              <input
+                type="text"
+                className="border p-2 text-black"
+                placeholder="Search for a player..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+              <button
+                className="bg-blue-500 text-white p-2 ml-2"
+                onClick={handleSearch}
+              >
+                Search
+              </button>
+            </div>
+            <ul>
+              {searchResults.map((player, index) => (
+                <li key={index} className="mb-2">
+                  <div>ID: {player.id}</div>
+                  <div>Name: {player.username}</div>
+                  <div>
+                    Progress:{" "}
+                    {player.perfs[selectedCategory]
+                      ? player.perfs[selectedCategory].progress
+                      : ""}
+                  </div>
+                  <div>
+                    Rating:{" "}
+                    {player.perfs[selectedCategory]
+                      ? player.perfs[selectedCategory].rating
+                      : ""}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </div>
+      </div>
 
-        <section>
-          <h2 className="text-xl mt-8 mb-2">Choose Category</h2>
-          <select
-            className="border p-2 text-black"
-            value={selectedCategory}
-            onChange={(e) => {
-              console.log(e.target.value);
-              setSelectedCategory(e.target.value);
-            }}
-          >
-            <option value="bullet">Bullet</option>
-            <option value="blitz">Blitz</option>
-            <option value="rapid">Rapid</option>
-            <option value="classical">Classical</option>
-            <option value="ultraBullet">Ultra Bullet</option>
-            <option value="chess960">Chess960</option>
-            <option value="crazyhouse">Crazy House</option>
-            <option value="antichess">Antichess</option>
-            <option value="atomic">Atomic</option>
-            <option value="horde">Horde</option>
-            <option value="kingOfTheHill">King Of The Hill</option>
-            <option value="racingKings">Racing Kings</option>
-            <option value="threeCheck">Three Check</option>
-            {/* Add other categories here */}
-          </select>
-        </section>
+      {/* Top 10 Players */}
+      <section className="flex flex-col items-center justify-center">
+        <h2 className="text-3xl mt-8 mb-2">Top 10 Players 🏆</h2>
+        {/* <ul>
+          {topPlayersData.map((player, index) => (
+            <li key={index} className="mb-2">
+              <div>ID: {player.id}</div>
+              <div>Name: {player.username}</div>
+              <div>
+                Progress:{" "}
+                {player.perfs[selectedCategory]
+                  ? player.perfs[selectedCategory].progress
+                  : ""}
+              </div>
+              <div>
+                Rating:{" "}
+                {player.perfs[selectedCategory]
+                  ? player.perfs[selectedCategory].rating
+                  : ""}
+              </div>
+              <div>
+                {getLinks(player.id).then((links) => (
+                  <div>
+                    <div>URL: {links.url}</div>
+                    {links.playing && <div>Playing: {links.playing}</div>}
+                    {links.twitch && (
+                      <div>
+                        Twitch:{" "}
+                        <a
+                          href={links.twitch}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {links.twitch}
+                        </a>
+                      </div>
+                    )}
+                    {links.youtube && (
+                      <div>
+                        YouTube:{" "}
+                        <a
+                          href={links.youtube}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {links.youtube}
+                        </a>
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
+            </li>
+          ))}
+        </ul> */}
+        <ul className="flex flex-wrap space-x-10 items-center justify-center">
+          {topPlayersData.map((player, index) => (
+            <li key={index} className="mb-4 border rounded p-4">
+              <div>ID: {player.id}</div>
+              <div>Name: {player.username}</div>
 
-        {/* Top 10 Players */}
-        <section>
-          <h2 className="text-xl mt-8 mb-2">Top 10 Players</h2>
-          <ul>
-            {topPlayersData.map((player, index) => (
-              <li key={index} className="mb-2">
-                <div>Name: {player.id}</div>
-                <div>Name: {player.username}</div>
+              <div>
+                Progress:{" "}
+                {player.perfs[selectedCategory]
+                  ? player.perfs[selectedCategory].progress
+                  : ""}
+              </div>
+              <div>
+                Rating:{" "}
+                {player.perfs[selectedCategory]
+                  ? player.perfs[selectedCategory].rating
+                  : ""}
+              </div>
 
-                <div>
-                  Progress:{" "}
-                  {player.perfs[selectedCategory]
-                    ? player.perfs[selectedCategory].progress
-                    : ""}
-                </div>
-                <div>
-                  Rating:{" "}
-                  {player.perfs[selectedCategory]
-                    ? player.perfs[selectedCategory].rating
-                    : ""}
-                </div>
-              </li>
-            ))}
-          </ul>
-        </section>
+              <div>
+                {getLinks(player.id).then((links) => (
+                  <div>
+                    <div>URL: {links.url}</div>
+                    {links.playing && <div>Playing: {links.playing}</div>}
+                    {links.twitch && (
+                      <div>
+                        Twitch:{" "}
+                        <a
+                          href={links.twitch}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {links.twitch}
+                        </a>
+                      </div>
+                    )}
+                    {links.youtube && (
+                      <div>
+                        YouTube:{" "}
+                        <a
+                          href={links.youtube}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {links.youtube}
+                        </a>
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
 
-        {/* Livestreams Section */}
-        <section>
-          <h2 className="text-xl mt-8 mb-2">Random Livestreams</h2>
-          <ul>
-            {livestreams.map((stream, index) => (
-              <li key={index} className="mb-4">
-                <div>Name: {stream.name}</div>
-                <div>Headline: {stream.streamer.headline}</div>
-                <div>
-                  {stream.streamer.twitch && (
-                    <a
-                      href={stream.streamer.twitch}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Twitch
-                    </a>
-                  )}
-                  {stream.streamer.youTube && (
-                    <a
-                      href={stream.streamer.youTube}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      YouTube
-                    </a>
-                  )}
-                </div>
-              </li>
-            ))}
-          </ul>
-        </section>
-      </main>
-
-      {/* Footer */}
-      <footer className="text-center py-4 bg-gray-200 w-full">
-        {/* Footer content */}
-      </footer>
+      {/* Livestreams Section */}
+      <section className="flex flex-col items-center justify-center">
+        <h2 className="text-3xl mt-8 mb-2">
+          Random Livestreams 🎥 - Clickable Links 👇
+        </h2>
+        <ul className="flex flex-wrap space-x-10 items-center justify-center">
+          {livestreams.map((stream, index) => (
+            <li key={index} className="mb-4 border rounded p-4">
+              <div>Name: {stream.name}</div>
+              <div>Headline: {stream.streamer.headline}</div>
+              <div className="flex space-x-5">
+                {stream.streamer.twitch && (
+                  <a
+                    href={stream.streamer.twitch}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-purple-500"
+                  >
+                    Twitch
+                  </a>
+                )}
+                {stream.streamer.youTube && (
+                  <a
+                    href={stream.streamer.youTube}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-red-500"
+                  >
+                    YouTube
+                  </a>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 };
